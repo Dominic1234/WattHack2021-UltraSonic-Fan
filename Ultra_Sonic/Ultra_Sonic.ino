44479cee909c40b4295ad5807f22722888d650a9
Dhruv Sharma <dhruv.sharma.017@gmail.com>
2021-11-23 11:53:55 +1100

    fixed issues with direction of fan

--- Ultra_Sonic/Ultra_Sonic.ino
+++ Ultra_Sonic/Ultra_Sonic.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
-#define RIGHT_ANGLE 170
-#define LEFT_ANGLE 0
+#define LEFT_ANGLE 180
+#define RIGHT_ANGLE 0
 #define REST_ANGLE 90
 
+#define MIN_DIST 2
 #define MAX_DIST 100
+
+#define RIGHT_LED 8
+#define LEFT_LED 4
+#define MIDDLE_LED 5
 
 //
 ////Servo declarationss
@@ -15,7 +20,7 @@
 
  // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 13, en = 10, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+const int rs = 13, en = 10, d4 = A5, d5 = A4, d6 = A3, d7 = A2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 //US sensor declarations
@@ -48,7 +53,6 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
 void calc_angle(double curr_ang, double new_ang) { 
   if(curr_ang > new_ang) {
     Serial.println("moving right");
-    lcd.print("moving right");
     for(double i = curr_ang; i > new_ang; i--) {
       servo_9.write(i);
       delay(25);
@@ -56,14 +60,12 @@ void calc_angle(double curr_ang, double new_ang) {
   }
   else if(curr_ang < new_ang){
     Serial.println("moving left");
-    lcd.print("moving left");
     for(double i = curr_ang; i < new_ang; i++) {
       servo_9.write(i);
       delay(25);
     }
   }
   else {
-    lcd.print("at rest");
     return;
   }
 }
@@ -91,68 +93,75 @@ void loop() {
   //US sensor code
   // put your main code here, to run repeatedly:
   //meausre ping time in cm
-  cm_1 = 0.01723 * readUltrasonicDistance(7,6);
-  cm_2 = 0.01723 * readUltrasonicDistance(12,11);
-  cm_3 = 0.01723 * readUltrasonicDistance(2,3);
+  cm_1 = 0.01723 * readUltrasonicDistance(7,6); //middle
+  cm_2 = 0.01723 * readUltrasonicDistance(12,11); //right
+  cm_3 = 0.01723 * readUltrasonicDistance(2,3); // left
   distance_arr[0] = cm_1;
   distance_arr[1] = cm_2;
   distance_arr[2] = cm_3;
 
   //[cm1, cm2. cm3]
-  
   min_val = cm_1;
   my_indx = 0;
 
   for(int i = 0; i < 3; i++){
-    if ( distance_arr[i] < min_val && distance_arr[i] < MAX_DIST){
-      min_val = distance_arr[i];
-      my_indx = i;
-    }
+      if ( distance_arr[i] < min_val){
+        min_val = distance_arr[i];
+        my_indx = i;
+      }
   }
+
+  
   if(min_val < MAX_DIST){
     
-ang = servo_9.read();
-if( my_indx == 0){          //right
-  calc_angle(ang, REST_ANGLE);
-}  else if  (my_indx == 1){ //center
-  calc_angle(ang, LEFT_ANGLE);
-}else {                     //left
-  calc_angle(ang, RIGHT_ANGLE);
-}
-
-//if( my_indx == 0){
-//  servo_9.write(90);
-//}  else if  (my_indx == 1){
-//  servo_9.write(0);
-//}else {
-//  servo_9.write(170); 
-//}
-
- //(my_indx == 2)
-  
-    //Serial.print("min value is");
-    //Serial.println(min_val);
-//    Serial.print("minimum index is");
-//    Serial.println(my_indx);
+    ang = servo_9.read();
+    if( my_indx == 0){          //center
+        lcd.clear();
+        lcd.print("centering");
+      calc_angle(ang, REST_ANGLE);
+    }  else if  (my_indx == 1){ //right
+        lcd.clear();
+        lcd.print("moving right");
+      calc_angle(ang, RIGHT_ANGLE);
+    }else {                     //left
+      lcd.clear();
+      lcd.print("moving left");
+      calc_angle(ang, LEFT_ANGLE);
+    }
     
-  
-  //convert to inches 
-  //inches = (cm/2.54);
-
-}else{
-  lcd.print("cannot find you!");
-  servo_9.write(ang);
-  //to add blinking lights if all sensors out of maximum set range
-  }
-
-  //Servo Code
-
-  Serial.print(cm_1);
-  Serial.print(",");
-  Serial.print(cm_2);
-  Serial.print(",");
-  Serial.println(cm_3);
-  Serial.println(cm_3);
-
-  delay(500);
+    //if( my_indx == 0){
+    //  servo_9.write(90);
+    //}  else if  (my_indx == 1){
+    //  servo_9.write(0);
+    //}else {
+    //  servo_9.write(170); 
+    //}
+    
+     //(my_indx == 2)
+      
+        //Serial.print("min value is");
+        //Serial.println(min_val);
+    //    Serial.print("minimum index is");
+    //    Serial.println(my_indx);
+        
+      
+      //convert to inches 
+      //inches = (cm/2.54);
+    
+    }else{
+      lcd.print("cannot find you!");
+      servo_9.write(ang);
+      //to add blinking lights if all sensors out of maximum set range
+      }
+    
+      //Servo Code
+    
+      Serial.print(cm_1);
+      Serial.print(",");
+      Serial.print(cm_2);
+      Serial.print(",");
+      Serial.println(cm_3);
+      Serial.println(cm_3);
+    
+      delay(500);
 }
